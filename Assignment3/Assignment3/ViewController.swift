725f1a4a98a3712f59dd5480445cd2bf6d7edfc1
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-10-28 21:38:22 +0200

    Initial commit
    A decent, setup blank project

---
+++ Assignment3/Assignment3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment3
+//
+//  Created by Volodya on 28.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
